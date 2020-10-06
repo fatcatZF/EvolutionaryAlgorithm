e47595d68f2a4016b1fb7ac6abd49db438c86baf
fatcatZF <z.fang0512@gmail.com>
2020-10-06 18:41:15 +0200

    tournament size

--- Assignment1/code.py
+++ Assignment1/code.py
@@ -39,8 +39,38 @@ def crossover_uniform(parent1, parent2, cross_points):
 
 
 
+def selection_p(problem, x, population):
+    """
+    proportional mating selection
+    """
+    fit = np.array([problem(individual) for individual in x])
+    fit_scaled = fit -np.min(fit)
+    selection_probability = fit_scaled/np.sum(fit_scaled)
+    x_prime_index = np.random.choice(range(len(x)),population, replace=True, p=selection_probability)
+    x_prime = x[x_prime_index]
+    couples = [[x_prime[i-1],x_prime[i]] for i in range(1, len(x_prime),2)]
+    return couples
 
-def studentname1_studentname2_GA(problem, pc_init=0.9, pc_min=0.6, pm_init=1/dimension, pm_max=0.1,
+
+def selection_t(problem, x, population , n=10):
+    """
+    tournament selection
+    n: size of tournament
+    """
+    x_prime = []
+    for i in range(population):
+        tournament = np.random.choice(np.arange(len(x)),n, replace=True)
+        selected = max(x[tournament],key=problem)
+        
+        x_prime.append(selected)
+    couples = [[x_prime[i-1],x_prime[i]] for i in range(1, len(x_prime),2)]
+   
+    return couples
+    
+
+
+def studentname1_studentname2_GA(problem, pc_init=0.9, pc_min=0.6, pm_init=1/dimension, 
+                                pm_max=0.1, selection=selection_t,
                                 crossover = crossover_uniform, crossover_size = 5, 
                                 is_adaptive=True, population=100):
     """
@@ -63,9 +93,7 @@ def studentname1_studentname2_GA(problem, pc_init=0.9, pc_min=0.6, pm_init=1/dim
 
     #Initialize the parent population. 250 parents
     x = np.array([np.random.rand(n) < 0.5 for i in range(population)]) 
-    #Evaluate parent population
-    #fit = np.array([problem(individual) for individual in x])
-    #fit_scaled = fit - np.min(fit)
+    
     
     if is_adaptive:
         step_pc = (pc_init-pc_min)/budget
@@ -81,29 +109,28 @@ def studentname1_studentname2_GA(problem, pc_init=0.9, pc_min=0.6, pm_init=1/dim
         if is_adaptive:
             pc = max(pc_min, pc_init-run_time*step_pc)
             pm = min(pm_max, pm_init+run_time*step_pm)
-        fit = np.array([problem(individual) for individual in x])
-        fit_scaled = fit - np.min(fit)
+       # fit = np.array([problem(individual) for individual in x])
+       # fit_scaled = fit - np.min(fit)
         #Select the individuals to mate depending on their fitness
-        selection_probability = fit_scaled/np.sum(fit_scaled)
-        x_prime_index = np.random.choice(range(len(x)), len(x), replace = True, p = selection_probability)
+        #selection_probability = fit_scaled/np.sum(fit_scaled)
+       # x_prime_index = np.random.choice(range(len(x)), len(x), replace = True, p = selection_probability)
         #The same parent can be choiced 
-        x_prime = x[x_prime_index]
-        x_prime = np.random.permutation(x_prime)
+       # x_prime = x[x_prime_index]
         #Generate a population from the crossing over between selected individuals.
-        couples = [[x_prime_index[i-1],x_prime_index[i]] for i in range(1, len(x_prime_index),2)]
-    
+       # couples = [[x_prime_index[i-1],x_prime_index[i]] for i in range(1, len(x_prime_index),2)]
+        couples = selection(problem,x,population)
         x_prime_prime = []
         for couple in couples:
             if np.random.random() < pc: #Probability of crossover
                 cross_points = np.sort(np.random.randint(99, size=crossover_size))
-                child1, child2 = crossover(x_prime[couple[0]], x_prime[couple[1]], cross_points)
+                child1, child2 = crossover(couple[0], couple[1], cross_points)
                 x_prime_prime.append(child1) 
                 x_prime_prime.append(child2)
                 crossover_times+=1
             
             else:
-                x_prime_prime.append(x_prime[couple[0]]) 
-                x_prime_prime.append(x_prime[couple[1]])
+                x_prime_prime.append(couple[0]) 
+                x_prime_prime.append(couple[1])
                 
         x_prime_prime = np.array(x_prime_prime)
                
@@ -151,7 +178,7 @@ if __name__ == '__main__':
     crossover = crossover_uniform if input("Please input crossover function(uniform/npoints): ").lower()=="uniform" else crossover_n
     if crossover==crossover_n:
        crossover_size = int(input("please input crossover size: "))
-       args['crossoer_size']=crossover_size
+       args['crossover_size']=crossover_size
     
     #the args of GA function
     args.update({'is_adaptive':is_adaptive, 'pc_init':pc_init, 'pm_init':pm_init, 'crossover':crossover, 'population':population})
@@ -168,7 +195,8 @@ if __name__ == '__main__':
         problem.reset()
         #print(' ' + 'run {}/{}'.format(i + 1, independent_runs) + '...', end=' ')
         fopt = studentname1_studentname2_GA(problem, **args)
-        print(fopt)
+        num_evaluations = problem.evaluations
+        print("score: ",fopt," number of evaluations: ",num_evaluations)
 
     problem.add_logger(logger)
     
